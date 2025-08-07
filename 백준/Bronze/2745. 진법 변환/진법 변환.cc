0ec17ce90455618c5007a7ceca1cca0b835fbafc
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-08-07 14:42:14 +0900

    Update 진법 변환.cc

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -5,8 +5,10 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+
     int b;
-    int dec = 0;
+    int res = 0;
     string n;
     cin >> n >> b;
 
@@ -15,12 +17,10 @@ int main()
         int num;
 
         if (n[i] >= '0' && n[i] <= '9') 
-            num = n[i] - '0';
+            res += (n[i] - '0')* pow(b, n.length() - (i + 1));
         else if (n[i] >= 'A' && n[i] <= 'Z') // 10진법 이상인 경우
-            num = n[i] - 55;
-
-        dec += num * pow(b, n.length() - (i + 1)); // 
+            res += (n[i] - 'A' + 10) * pow(b, n.length() - (i + 1));
     }
 
-    cout << dec;
+    cout << res;
 }
