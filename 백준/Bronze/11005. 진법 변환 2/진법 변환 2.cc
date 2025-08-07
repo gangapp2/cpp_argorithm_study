07964fe2b691fdb6b3a20902537e67d88093dd30
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-08-07 15:20:58 +0900

    Update 진법 변환 2.cc
    수정 완

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -4,23 +4,22 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    int n,b;
-    string res;
+	int n, b;
+	cin >> n >> b;
+	string res;
 
-    cin >> n >> b;
+	while (n > 0)
+	{
+		int temp = n % b;
 
-    while (n>0)
-    {
-        int temp = n % b;
-        if (temp >= 0 && temp <= 9)
-            res += (temp + '0');
-        else
-            res += (temp - 10 + 'A');
-        n /= b;
-    }
+		if (temp >= 0 && temp <= 9)
+			res += (temp + '0'); // 아스키코드 '0'~'9'로 변환해주어야 하므로 'a'만큼 더해준다
+		else
+			res += (temp - 10 + 'A'); // 10이 출발점이므로 temp-10 이후 + 'A'
 
-    for (int i = res.length()-1;i>=0; i--)
-        cout << res[i];
+		n /= b; // 나눠진 몫으로 n을 초기화
+	}
 
+	for(int i = res.length() - 1; i >= 0; i--) // 역순출력
+		cout << res[i];
 }
