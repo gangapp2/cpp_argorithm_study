8a96656d5e38279c0b0c4b3d8bd9bf249999530c
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-09-07 12:58:05 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -3,17 +3,16 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-
 	int a, b, v;
-	int day = 1;
 	cin >> a >> b >> v;
 
-	// 목표 거리를 하루에 가는 거리로 나누어 소요되는 총 일수를 구한다
-	day += (v - a) / (a - b); // v-a = 도착 목표 지점, a-b = 하루에 가는 거리
+	int day = 1;
 
-	if ((v - a) % (a - b)) // 달팽이가 도착점을 지나치는 경우는 날짜가 하루 더 소요됨
-		cout << day + 1; //
+	day += (v - a) / (a - b);
+
+	if ((v - a) % (a - b))
+		cout << day + 1;
 	else
-		cout << day;	// 달팽이가 도착점에 딱 맞게 도달하는 경우
+		cout << day;
+
 }
