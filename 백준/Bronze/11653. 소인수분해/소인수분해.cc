fea635146a13bb09e1e45d39b6b83bf3d7740353
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-09-08 16:40:27 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,22 +1,29 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int n;
-	int i = 2;
-	cin >> n;
+    long long n;
+    if (!(cin >> n)) return 0;
 
-	while (n != 1)
-	{
-		if (n % i == 0)
-		{
-			n /= i;
-			cout << i << endl;
-		}
-		else
-			i++;
-	}
+    // 1) 2로 나눌 수 있을 만큼 먼저 모두 나누기
+    while (n % 2 == 0) {
+        cout << 2 << '\n';
+        n /= 2;
+    }
+
+    // 2) 홀수만 검사: i*i <= n까지만
+    for (long long i = 3; i * i <= n; i += 2) {
+        while (n % i == 0) {
+            cout << i << '\n';
+            n /= i;
+        }
+    }
+
+    // 3) 남은 n이 1보다 크면 n 자체가 마지막 소수
+    if (n > 1) cout << n << '\n';
+
+    return 0;
 }
