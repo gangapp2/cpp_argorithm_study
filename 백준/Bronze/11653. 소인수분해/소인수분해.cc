9042794bbda0a2c09fd560d5eadcefc54a905932
gan9app2 <kkh994711@gmail.com>
2025-09-08 17:24:57 +0900

    풀이추가

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,29 +1,37 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main()
+{
+	ios::sync_with_stdio(false);
 
-    long long n;
-    if (!(cin >> n)) return 0;
+	long long n;
+	cin >> n;
 
-    // 1) 2로 나눌 수 있을 만큼 먼저 모두 나누기
-    while (n % 2 == 0) {
-        cout << 2 << '\n';
-        n /= 2;
-    }
+	// 짝수 인수를 모두 제거
+	while (n % 2 == 0)
+	{
+		cout << 2 << endl;
+		n /= 2;
+	}
 
-    // 2) 홀수만 검사: i*i <= n까지만
-    for (long long i = 3; i * i <= n; i += 2) {
-        while (n % i == 0) {
-            cout << i << '\n';
-            n /= i;
-        }
-    }
+	// 홀수만 검사 : i<= 루트n까지
+	// n이 합성수일때 약수는 항상 쌍으로 나타남
+	// 항상 약수쌍의 작은쪽은 루트n이하, 큰쪽은 루트n이상임 
+	// i<=루트n 조건을 만족하는 약수가 더 이상 없는 경우 모든 약수를 검사했다는 뜻임
+	// ex) i > 루트n일때 다른 쌍은 반드시 루트n보다 작아야 하는데,
+	//  조건문을 벗어났다는 것은 이미 루트n보다 작은 수는 모두 검사가 끝났따는 뜻이므로 모순
 
-    // 3) 남은 n이 1보다 크면 n 자체가 마지막 소수
-    if (n > 1) cout << n << '\n';
+	for (int i = 3; i * i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			cout << i << endl;
+			n /= i;
+		}
+	}
 
-    return 0;
+	// 모든 약수가 걸러진 상태
+	if (n > 1)
+		cout << n << endl;
 }
