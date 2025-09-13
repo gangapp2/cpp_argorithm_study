b4c1586f4c6a4aa7a00548129f86e379639cf097
gan9app2 <kkh994711@gmail.com>
2025-09-13 20:37:07 +0900

    풀이추가"

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+	// x, y 각 축 모두 같은값의 두개의 쌍을 가지고있어야함
 	int x[3] = {};
 	int y[3] = {};
 
@@ -11,17 +12,19 @@ int main()
 	{
 		cin >> x[i] >> y[i];
 	}
-	
+
+	// 같은 x축 쌍 걸러줌
 	if (x[0] == x[1])
 		cout << x[2] << " ";
-	else if(x[0] == x[2])
+	else if (x[0] == x[2])
 		cout << x[1] << " ";
 	else
 		cout << x[0] << " ";
 
+	// 같은 y축 쌍 걸러줌
 	if (y[0] == y[1])
 		cout << y[2];
-	else if (y[0] ==y[2])
+	else if (y[0] == y[2])
 		cout << y[1];
 	else
 		cout << y[0];
