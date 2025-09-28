143f1d6d584c33330b64971701f65319037f3624
gan9app2 <kkh994711@gmail.com>
2025-09-28 19:27:16 +0900

    해설추가

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -21,16 +21,15 @@ int main()
 			switch (j)
 			{
 			case 0:
-				dp[i][j] = dp[i - 1][1];
+				dp[i][j] = dp[i - 1][1] % MOD;
 				break;
 			case 9:
-				dp[i][j] = dp[i - 1][8];
+				dp[i][j] = dp[i - 1][8] % MOD;
 				break;
 			default:
-				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1] % MOD;
 				break;
 			}
-			dp[i][j] %= MOD;
 		}
 	}
 
@@ -38,5 +37,8 @@ int main()
 	for (int i = 0; i <= 9; i++)
 		answer += dp[n][i];
 
+	// (a+b)modM = ((amodM)+(bmodM))modM
+	// 각각의 숫자의 나머지를 먼저 구한후 전부 더해서 그 합의 나머지를 구한것과 : (a % m + b % m + c % m) % m
+	// 모든 수를 다 더한후 한꺼번에 나눠서 나머지를 구한 것의 결과는 동일하다 : (a + b + c) % m
 	cout << answer % MOD;
 }
