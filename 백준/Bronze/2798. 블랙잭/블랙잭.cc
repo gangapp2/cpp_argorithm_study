74795bd95d4f3ac2c058d516cfa29ab4940aafff
gan9app2 <kkh994711@gmail.com>
2025-10-30 21:38:03 +0900

    블랙잭 풀이

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -14,9 +14,10 @@ int main()
 		cin >> cards[i];
 
 	// 카드 3장의 합으로 나올 수 있는 모든 경우의 수 계산
-	for (int i = 0; i < n; i++)
+
+	for (int i = 0; i < n - 2; i++) // i의 범위는 뒤에서 3번째 카드가 최대임
 	{
-		for (int j = i + 1; j < n; j++)
+		for (int j = i + 1; j < n - 1; j++) // j의 범위는 뒤에서 2번째 카드가 최대임
 		{
 			for (int k = j + 1; k < n; k++)
 			{
