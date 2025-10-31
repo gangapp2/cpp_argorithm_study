62bc5d66ef49504a359b735c0e5df8376e928270
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-10-31 18:30:14 +0900

    [Silver III] Title: 숫자 야구, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.cc
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.cc
@@ -2,33 +2,28 @@
 #include<vector>
 #include<string>
 #include<cstring>
-
 using namespace std;
+
+bool Card_Num[1000];
 
 struct Game
 {
 	int num;
-	int s; //strike
-	int b; // ball
+	int s;
+	int b;
 };
-
-bool Can_Num[1000]; // 111~999까지 숫자들을 담을 배열
 vector<Game> Round;
-
-
 int main()
 {
-	// 자릿수 중복, 0이 포함되는 수들은 사용 불가
-	memset(Can_Num, true, sizeof(Can_Num));
+	memset(Card_Num, true, sizeof(Card_Num));
 	for (int i = 123; i < 1000; i++)
 	{
-		string tmp = to_string(i);
-		// 각 자릿수가 중복되면 false
-		if (tmp[0] == tmp[1] || tmp[0] == tmp[2] || tmp[1] == tmp[2])
-			Can_Num[i] = false;
-		// 0이 포함되면 flase
-		if(tmp[0] == '0' || tmp[1] == '0' || tmp[2] == '0')
-			Can_Num[i] = false;
+		string tmp = to_string(i); // 배열의 인덱스 값을 세자리 수로 사용중, 자릿수 비교에 용이하게 string으로 변경
+
+		if (tmp[0] == tmp[1] || tmp[0] == tmp[2] || tmp[1] == tmp[2]) // 자릿수가 중복되면 안됨
+			Card_Num[i] = false;
+		if (tmp[0] == '0' || tmp[1] == '0' || tmp[2] == '0') // 자릿수가 0이 될수 없음
+			Card_Num[i] = false;
 	}
 
 	int n;
@@ -42,41 +37,44 @@ int main()
 		cin >> g.b;
 		Round.push_back(g);
 	}
-
+	// 민혁이가 질문한 수의 볼카운트와  각 숫자들의 볼카운트가 일치하는지 비교
 	for (int i = 0; i < n; i++)
 	{
 		string num = to_string(Round[i].num);
 		int strike = Round[i].s;
 		int ball = Round[i].b;
 
-		for (int j = 123; j < 1000; j++) // 현재 라운드의 볼카운트가 일치하지 않는 수를 찾아낸다
+		for (int j = 123; j < 1000; j++)
 		{
-			int tmp_strike = 0;
-			int tmp_ball = 0;
-			if (Can_Num[j] == true)
+			if (Card_Num[j]) // card_num == true
 			{
-				string tmp = to_string(j); // 현재 라운드의 숫자와 비교할 숫자 = 인덱스값 j
+				string tmp_num = to_string(j);
+				int tmp_strike = 0;
+				int tmp_ball = 0;
+				// 볼카운트 비교
 				for (int a = 0; a < 3; a++)
 				{
 					for (int b = 0; b < 3; b++)
 					{
-						// strike인 경우
-						if (a == b && num[a] == tmp[b])
+						// strike
+						if (a == b && tmp_num[a] == num[b])
 							tmp_strike++;
-						// ball인 경우
-						if (a != b && num[a] == tmp[b])
+						// ball
+						if (a != b && tmp_num[a] == num[b])
 							tmp_ball++;
 					}
 				}
+				if (strike != tmp_strike || ball != tmp_ball)
+					Card_Num[j] = false;
 			}
-			if (strike != tmp_strike || ball != tmp_ball)
-				Can_Num[j] = false;
 		}
+	
 	}
-	int ans = 0;
+
+	int ans=0;
 	for (int i = 123; i < 1000; i++)
 	{
-		if (Can_Num[i])
+		if (Card_Num[i])
 			ans++;
 	}
 	cout << ans;
