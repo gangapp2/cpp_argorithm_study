2f6ea07e591f9dfd61cecfbaa03918cb889fc0e4
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-02 02:39:57 +0900

    [Gold V] Title: Divisibility Trick, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/30599. Divisibility Trick/Divisibility Trick.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	int N;
+	cin >> N;
+	to_string(N);
+	for (int i = 0; i < N; ++i) {
+		cout << N;
+	}
+	return 0;
+}
