e9740e69fad44b1f7ef4636896ed488251f4fc48
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-02 20:37:46 +0900

    [Gold V] Title: 삭삽 정렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/13884. 삭삽 정렬/삭삽 정렬.cc
+++ 백준/Gold/13884. 삭삽 정렬/삭삽 정렬.cc
@@ -1,52 +1,46 @@
-#include <iostream>
+#include<iostream>
 #include<vector>
 #include<algorithm>
 #include<climits>
 using namespace std;
 
-int T, N;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cout << fixed;
-    cout.precision(4);
+	int p;
+	cin >> p;
 
-    cin >> T;
+	while (p--)
+	{
+		int num, size;
+		cin >> num >> size;
 
-    while (T--)
-    {
-        int K;
-        cin >> K >> N;
+		vector<int> origin(size);
+		vector<int> rank(size); // 정렬된 배열
 
-        vector<int> origin(N);
-        vector<int> nums(N); // 정렬된 배열
+		int minVal = INT_MAX;
 
-        int minNum = INT_MAX;
-        for (int i = 0; i < N; i++)
-        {
-            cin >> nums[i];
-            origin[i] = nums[i];
-            minNum = min(minNum, nums[i]);
-        }
+		for (int i = 0; i < size; i++)
+		{
+			cin >> rank[i];
+			origin[i] = rank[i];
+			minVal = min(minVal, rank[i]);
+		}
 
-        sort(nums.begin(), nums.end());
+		sort(rank.begin(), rank.end());
 
-        //정렬되어 있는 숫자들 파악
-        int target = minNum;
-        int cursor = 0;
-        for (int i = 0; i < N; i++)
-        {
-            if (target == origin[i])
-            {
-                target = nums[++cursor];
-            }
-        }
+		int count = 0;
+		for (int i = 0; i < size; i++)
+		{
+			if (minVal == origin[i])
+			{
+				minVal = rank[++count];
+			}
+		}
 
-        cout << K << " " << N - cursor << "\n";
-    }
-
-
-    return 0;
+		cout << num << " " << size - count << "\n";
+	}
 }
