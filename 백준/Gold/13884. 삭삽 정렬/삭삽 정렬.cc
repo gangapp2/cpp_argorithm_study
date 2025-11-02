8c625b77507df0ce57cd4b4c582cc7ea0f5dccdd
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-02 19:43:49 +0900

    [Gold V] Title: 삭삽 정렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/13884. 삭삽 정렬/삭삽 정렬.cc
+++ 백준/Gold/13884. 삭삽 정렬/삭삽 정렬.cc
@@ -1,125 +1,52 @@
-#include<iostream>
+#include <iostream>
 #include<vector>
 #include<algorithm>
-#include <string>
+#include<climits>
 using namespace std;
 
+int T, N;
 
-struct dataset
-{
-	int num;
-	int size;
-	int ans;
-};
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout.precision(4);
 
-vector<dataset> dSet;
-vector<vector<int>> arr;
-vector<vector<int>> arr_tmp; // 오름차순으로 정렬된 배열 
-vector<vector<int>> arr_rank; // 중복이 제거된 순위 배열
+    cin >> T;
 
-int FindLastIndex(int min,vector<int> arr) // min값의 마지막 index를 리턴
-{
-	int index = -1;
-	for (int i = 0; i < arr.size(); i++)
-	{
-		if (arr[i] == min)
-			index = i;
-	}
-	return index;
-}
+    while (T--)
+    {
+        int K;
+        cin >> K >> N;
 
-int CountMin(int min, int lastIndex, vector<int> arr) // 이전 최솟값의 마지막 인덱스 뒤에 나오는 현재 최솟값의 개수를 카운트
-{
-	int count = 0;
-	for (int i = lastIndex + 1; i < arr.size(); i++)
-	{
-		if (min == arr[i])
-			count++;
-	}
-	return count;
-}
-int main()
-{
-	int p; // 테스트 케이스의 수
-	cin >> p;
+        vector<int> origin(N);
+        vector<int> nums(N); // 정렬된 배열
 
-	for (int i = 0; i < p; i++)
-	{
-		dataset d;
-		cin >> d.num;
-		cin >> d.size;
-		dSet.push_back(d);
-		arr.push_back({}); // 이중 벡터 초기화
-		arr_tmp.push_back({});
+        int minNum = INT_MAX;
+        for (int i = 0; i < N; i++)
+        {
+            cin >> nums[i];
+            origin[i] = nums[i];
+            minNum = min(minNum, nums[i]);
+        }
 
-		for (int j = 0; j < d.size; j++)
-		{
-			int n;
-			cin >> n;
-			arr[i].push_back(n);
-			arr_tmp[i].push_back(n);
-		}
-	}
-	// 중복제거한 순위배열 생성
-	for (int i = 0; i < p; i++)
-	{
-		sort(arr_tmp[i].begin(), arr_tmp[i].end());
-		arr_rank.push_back({}); // 이중 벡터 초기화
+        sort(nums.begin(), nums.end());
 
-		int temp = arr_tmp[i][0];
-		arr_rank[i].push_back(temp);
+        //정렬되어 있는 숫자들 파악
+        int target = minNum;
+        int cursor = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (target == origin[i])
+            {
+                target = nums[++cursor];
+            }
+        }
 
-		for (int j = 0; j < arr_tmp[i].size(); j++)
-		{
-			if (arr_tmp[i][j] != temp)
-			{
-				temp = arr_tmp[i][j];
-				arr_rank[i].push_back(temp);
-			}
-		}
-	}
+        cout << K << " " << N - cursor << "\n";
+    }
 
-	// 
-	for (int i = 0; i < p; i++)
-	{
-		int count = 0;
-		int index = -1;
-		int temp; 
-		bool isFound = false;
-		
-		for (int j = 0; j < arr_rank[i].size(); j++)
-		{
-			if (isFound)
-				break;
-			temp = arr_rank[i][j]; // 각 테스트 케이스의 최솟값
 
-			for (int k = 0; k < arr[i].size(); k++)
-			{
-				if (temp == arr[i][k]) // 최솟값 발견
-				{
-
-					if (index < k) // 순행
-					{
-						index = k;
-						count++;
-					}
-					else // 역행
-					{
-						int preMin = arr_rank[i][j - 1]; // 현재 최솟값의 이전 최솟값
-						int lastIndex = FindLastIndex(preMin, arr[i]);
-						count += CountMin(temp, lastIndex, arr[i]);
-
-						isFound = true;
-						dSet[i].ans = arr[i].size() - count;
-						break;
-					}
-				}
-			}
-		}
-	}
-
-	for (int i = 0; i < p; i++)
-	{
-		cout << dSet[i].num << " " << dSet[i].ans << endl;
-	}
+    return 0;
 }
