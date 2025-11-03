fe2f930089e7ca45abea7f53c82f680ff015380a
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-04 01:28:04 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -1,35 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <cstdio>
+#include<iostream>
+#include<vector>
 using namespace std;
- 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n;
-    vector<pair<int, int>> arr;
-    int now;
-    int next;
-    cin >> n;
-    int tmp;
-    for (int i = 0; i < n; i++) {
-        cin >> tmp;
-        arr.push_back(make_pair(tmp, i + 1));
-    }
-    now = 0;
-    while (arr.empty() != true) {
-        cout << arr[now].second;
-        next = arr[now].first;
-        if (next > 0) {
-            next -= 1;
-        }
-        arr.erase(arr.begin() + now);
-        now += next;
-        n--;
-        if (n <= 0) break;
-        now = (now >= 0 ? now : n + now % n);
-        now %= n;
-        cout << " ";
-    }
-    return 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	
+	int temp;
+	vector<pair<int, int>> v;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		v.push_back(make_pair(temp, i + 1)); // first = 종이 값, second = 풍선 번호
+	}
+
+	int index = 0;
+	int next;
+
+	while (!v.empty())
+	{
+		cout << v[index].second;
+		next = v[index].first;
+
+		if (next > 0) // 종이에서 나온 값이 양수면 한칸 적게 이동해야함(인덱스가 1 줄어든 상태이므로)
+			next -= 1;
+
+		v.erase(v.begin() + index);
+
+		int size = v.size();
+		if (size == 0)
+			break;
+		index += next;
+		
+		index = (index % size + size) % size;
+
+		cout << " ";
+	}
 }
