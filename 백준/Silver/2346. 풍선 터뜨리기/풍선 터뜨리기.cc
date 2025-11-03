cb56da7bb3896744c24b4d76762a5513ced0917c
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-04 01:03:12 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -1,49 +1,35 @@
 #include <iostream>
-#include <deque>
-#include <algorithm>
-
+#include <vector>
+#include <cstdio>
 using namespace std;
-
-int n,top,number;
-deque<pair<int, int>> dq;
-
+ 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> n;
-
-	for (int i = 1; i <= n; i++) {
-		int num;
-		cin >> num;
-		dq.push_back(make_pair(i, num));
-	}
-
-	number = dq.front().second;
-	cout << dq.front().first << " ";
-
-	dq.pop_front(); // 터진거 제거
-
-	while (!dq.empty()) {
-		if (number > 0) { // 양
-			for (int i = 0; i < number - 1; i++) { // 터진거 빼고 다시 deque생성
-				dq.push_back(dq.front());
-				dq.pop_front();
-			}
-			number = dq.front().second;
-			cout << dq.front().first << " ";
-			dq.pop_front();
-		}
-		else { // 음
-			for (int i = 0; i > number + 1; i--) { // 반대로 deque생성
-				dq.push_front(dq.back());
-				dq.pop_back(); // -1 2 1 -> 1 -1 2 1 -> 1 -1 2 -> 2 1 -1 2 -> 2 1 -1
-			}
-			number = dq.back().second;
-			cout << dq.back().first << " ";
-			dq.pop_back();
-		}
-
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    vector<pair<int, int>> arr;
+    int now;
+    int next;
+    cin >> n;
+    int tmp;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        arr.push_back(make_pair(tmp, i + 1));
+    }
+    now = 0;
+    while (arr.empty() != true) {
+        cout << arr[now].second;
+        next = arr[now].first;
+        if (next > 0) {
+            next -= 1;
+        }
+        arr.erase(arr.begin() + now);
+        now += next;
+        n--;
+        if (n <= 0) break;
+        now = (now >= 0 ? now : n + now % n);
+        now %= n;
+        cout << " ";
+    }
+    return 0;
 }
