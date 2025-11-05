3748094f6ce2bf4de6dc9d64c9f5103abd728154
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-06 00:38:49 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -4,6 +4,9 @@
 using namespace std;
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	int t;
 	cin >> t;
 
@@ -29,17 +32,17 @@ int main()
 
 			if (balance < 0)
 			{
-				cout << "NO" << endl;
+				cout << "NO" << "\n";
 				isFound = true;
 				break;
 			}
 		}
 		if (balance == 0)
-			cout << "YES" << endl;
+			cout << "YES" << "\n";
 		else
 		{
 			if(!isFound)
-				cout << "NO" << endl;
+				cout << "NO" << "\n";
 		}
 	}
 	return 0;
