a4b7a11e925cb5af183fc39f14d924a35361c9e3
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-10 21:18:44 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+stack<int> S;
+int N, T, cnt=1;
+
+int main(){	
+	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	cin >> N; // 학생들의 수 
+	
+	while(N--){
+		cin >> T;
+		
+		if(T==cnt) 
+            cnt++; 
+		else 
+            S.push(T); // 현재 순번과 일치하지 않으면 스택에서 대기ㅏ
+		
+		while(!S.empty()&&S.top()==cnt){
+			S.pop(); 
+			cnt++;
+		}
+	}
+	
+	if(S.empty()) cout << "Nice";
+	else cout << "Sad";
+}
