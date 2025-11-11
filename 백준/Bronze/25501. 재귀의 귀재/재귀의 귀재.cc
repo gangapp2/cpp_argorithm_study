226778479152a50c05cf803204cea2355ebb6ca7
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-11 13:12:52 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int cnt = 0;
+int recursion(string &s, int l, int r) 
+{
+    cnt++;
+    if (l >= r) return 1;
+    else if (s[l] != s[r]) return 0;
+    else return recursion(s, l + 1, r - 1);
+}
+
+int isPalindrome(string &s) 
+{
+    return recursion(s, 0, s.size()-1);
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        cout << isPalindrome(s) << " " << cnt << '\n';
+        cnt = 0;
+    }
+}
