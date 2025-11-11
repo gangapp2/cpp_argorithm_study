2693f5d94451801c6a10ce44bb0cad1300cf5c18
gan9app2 <kkh994711@gmail.com>
2025-11-11 15:01:56 +0900

    칸토어 집합/칸토어 집합.cc

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -6,11 +6,11 @@ using namespace std;
 string s;
 int arrSize;
 
-void Cantor(string &s, int left, int right)
+void Cantor(string& s, int left, int right)
 {
 	int arrSize = (right - left) + 1;
-	arrSize /= 3;
-	if (arrSize == 0)
+	arrSize /= 3; // 전역변수로 했었음 >> 한 번 나머지 계산이 되고나면 size를 되돌릴 수 없으므로 부적합
+	if (arrSize == 0) // 0일 경우 예외 처리
 		return;
 	// 가운데 범위
 	left = left + arrSize;
@@ -21,14 +21,14 @@ void Cantor(string &s, int left, int right)
 		s[i] = ' ';
 	}
 
-	if (left == right)
+	if (left == right) // 인덱스가 같다 >> 가운데 값이 하나다 >> 더이상 재귀호출할 필요x
 		return;
 
 	left = left - 1;
 	right = right + 1;
 
 	Cantor(s, left - (arrSize - 1), left);
-	Cantor(s, right, right+(arrSize - 1));
+	Cantor(s, right, right + (arrSize - 1));
 }
 
 int main()
@@ -38,7 +38,7 @@ int main()
 
 	int n;
 
-	while (cin>>n)
+	while (cin >> n)
 	{
 
 		arrSize = pow(3, n);
@@ -53,5 +53,5 @@ int main()
 		}
 		cout << '\n';
 	}
-	
+
 }
