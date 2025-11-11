a8de6e5bf4b696d50a6948aa9f6a8ae726d3c8af
Kim Kyeong Hyeob <120311813+gangapp2@users.noreply.github.com>
2025-11-11 12:05:04 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+long long ans = 1;
+
+void Factorial(int n)
+{
+	if (n <= 0) // 조건 : N>=0
+		return;
+
+	ans *= n;
+	Factorial(--n);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	Factorial(n);
+	cout << ans;
+	return 0;
+}
